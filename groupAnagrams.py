56f41c64c122395ee3a556dbab219e064db95c5e
valuntiny <459201296@qq.com>
2018-11-21 19:16:45 -0500

    list can not be the key of a dict, so we transfer it to a tuple

---
+++ groupAnagrams.py
@@ -0,0 +1,31 @@
+'''
+Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+
+All inputs will be in lowercase.
+The order of your output does not matter.
+'''
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        res = {}
+        for s in strs:
+            key = tuple(sorted(s)) # list can not be the key of a dict, so we transfer it to a tuple
+            res[key] = res.get(key, []) + [s]
+
+        return res.values()
